8ab583b37993687e7148ca6b9e79dbc3f955e890
Vrishank Viswanath <vrishank@utexas.edu>
2023-01-15 17:09:03 -0600

    replaced favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,15 +6,6 @@
 	export let data;
 </script>
 
-<!-- <div class="box-border p-8 h-screen w-screen bg-slate-10">
-	<Header />
-
-	{#key data.currentRoute}
-		<main class="" >
-			<slot />
-		</main>
-	{/key}
-</div> -->
 <main class="p-8" >
 	<slot />
 </main>
