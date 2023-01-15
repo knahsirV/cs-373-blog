ac6ff24def306d2bc33df030ad6396a2b427bb8d
Vrishank Viswanath <vrishank@utexas.edu>
2023-01-15 16:50:00 -0600

    netlify help

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex'
+import adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
