ea9b392288143b84a782337e7c3499bd933a5595
Vrishank Viswanath <vrishank@utexas.edu>
2023-01-15 16:53:00 -0600

    removed duplicate adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex'
 import adapter from '@sveltejs/adapter-netlify'
 
